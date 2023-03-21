dc219a9740aa5b25cc52aa67348a1c2e4eee10c9
thegamershollow <thegamershollow@outlook.com>
2023-03-21 17:11:40 +0000

    added the startup menu for GPT Terminal

---
+++ startup.py
@@ -0,0 +1,28 @@
+import os
+import openai
+import datetime
+import base64
+import math
+
+#imliments an OpenAI API key from API-Key.env
+apiKey = open('API-Key.env', 'r')
+api = apiKey.read()
+openai.api_key = api
+os.system("clear")
+
+gptSelection = input("Select an option from below by typing the number\n1. Open ChatGPT\n2. Open Jailbroken ChatGPT\n3. Options\n4. Exit\nInput: ")
+
+if gptSelection == '1':
+    os.system("clear")
+    os.system("python gpt.py")
+
+if gptSelection == '2':
+    os.system("clear")
+    os.system("python options.py")
+
+if gptSelection == '3':
+    os.system("clear")
+    os.system("python jailbreak.py")
+
+if gptSelection == '4':
+    print("Exited GPT Terminal")
